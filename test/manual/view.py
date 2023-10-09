91ad6a684096961eaf2097bfc57fe3180e30dcc4
Gennadiy <renardes@gmail.com>
2023-10-09 21:14:54 +0500

    ручной тест GUI: вычисляемый размер приложения

--- test/manual/view.py
+++ test/manual/view.py
@@ -12,12 +12,14 @@ class RootWidget(Tk):
         super().__init__()
         self.title('Тамагочи')
 
-        width = 600
-        height = 900
-        x = self.winfo_screenwidth() // 2 - width // 2
-        y = self.winfo_screenheight() // 2 - height // 2
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        self.width = screen_width // 3
+        self.height = screen_height * 3 // 4
+        x = screen_width // 2 - self.width // 2
+        y = screen_height // 2 - self.height // 2
 
-        self.geometry(f'{width}x{height}+{x}+{y}')
+        self.geometry(f'{self.width}x{self.height}+{x}+{y}')
         self.resizable(False, False)
 
         self.mainframe: Frame = None
@@ -32,61 +34,69 @@ class MainMenu(Frame):
     """"""
     def __init__(self, master: RootWidget, kinds: controller.LoadKinds[model.Kind]):
         super().__init__(master)
+        pad = master.width // 100 + 1
         self.grid(
             row=0, column=0,
-            padx=10, pady=10,
+            padx=pad, pady=pad,
             sticky='nsew',
         )
         columns = 2
+        img_size = (master.width - pad*2*(columns+1)) // columns - 10
         self._images: list[PhotoImage] = []
         for i, kind in enumerate(kinds):
             self._images.append(PhotoImage(
+                width=img_size, height=img_size,
                 file=kind.image,
-                width=260, height=260
             ))
             row, column = divmod(i, columns)
-            Button(
+            btn = Button(
                 self,
                 image=self._images[i],
                 # для теста:
                 command=lambda: master.change_frame(Game(master, yara)),
                 # на перспективу:
                 # command=lambda: master.change_frame(Game(master, controller.MainMenu.choose_kind(kind))),
-            ).grid(
+            )
+            btn.grid(
                 row=row, column=column,
                 sticky='nsew',
-                padx=10, pady=10,
+                padx=pad, pady=pad,
             )
 
 class Game(Frame):
     """"""
     def __init__(self, master: RootWidget, origin: model.Creature):
         super().__init__(master)
+        pad = (master.width // 100 + 1) * 2
+        ipad = pad // 4
         self.grid(
             row=0, column=0,
             sticky='nsew',
-            padx=20, pady=20,
+            padx=pad, pady=pad,
         )
-        self.rowconfigure(0, minsize=175)
-        self.rowconfigure(1, minsize=560)
-        self.rowconfigure(2, minsize=85)
-        self.columnconfigure(0, minsize=560)
+        self._screen_size = master.width - pad * 2
+        self._actions_height = (master.height - self._screen_size - pad*4) // 3
+        self._text_height = self._actions_height * 2
+        self.rowconfigure(0, minsize=self._text_height)
+        self.rowconfigure(1, minsize=self._screen_size)
+        self.rowconfigure(2, minsize=self._actions_height)
+        self.columnconfigure(0, minsize=self._screen_size)
 
         text_panel = Frame(self)
         text_panel.grid(
             row=0, column=0,
             sticky='nsew',
-            pady=(0, 20),
+            pady=(0, pad),
         )
-        text_panel.rowconfigure(0, minsize=175)
-        text_panel.columnconfigure(0, minsize=400)
-        text_panel.columnconfigure(1, minsize=160)
+        text_panel.rowconfigure(0, minsize=self._text_height)
+        text_panel.columnconfigure(0, minsize=self._screen_size//7*5)
+        text_panel.columnconfigure(1, minsize=self._screen_size//7*2)
 
         self.message = StringVar(self, '')
         Label(
             text_panel,
             textvariable=self.message,
-            wraplength=400,
+            wraplength=self._screen_size//7*5,
             font=('Arial Narrow', 16, 'italic'),
             anchor='nw',
             justify='left',
@@ -94,14 +104,14 @@ class Game(Frame):
         ).grid(
             row=0, column=0,
             sticky='nsew',
-            ipadx=5, ipady=5,
+            ipadx=ipad, ipady=ipad,
         )
 
         self.params = StringVar(self, '')
         Label(
             text_panel,
             textvariable=self.params,
-            wraplength=160,
+            wraplength=self._screen_size//7*2,
             font=('Consolas', 16, 'bold'),
             anchor='ne',
             justify='right',
@@ -109,7 +119,7 @@ class Game(Frame):
         ).grid(
             row=0, column=1,
             sticky='nsew',
-            ipadx=5, ipady=5,
+            ipadx=ipad, ipady=ipad,
         )
 
         self._image: PhotoImage = None
@@ -117,7 +127,7 @@ class Game(Frame):
         self.screen.grid(
             row=1, column=0,
             sticky='nsew',
-            pady=(0, 20),
+            pady=(0, pad),
         )
 
         self.create_buttons(origin)
@@ -128,17 +138,18 @@ class Game(Frame):
             row=2, column=0,
             sticky='nsew',
         )
-
+        buttons = 6
         self.actions: list[Button] = []
         self._buttons_images: list[PhotoImage] = []
-        paddings = (11, 11, 11, 11, 11, 0)
+        paddings = ((self._screen_size - self._actions_height*6)//(buttons-1),)*(buttons-1) + (0,)
+        img_size = self._actions_height - 10
         for i, pad in enumerate(paddings):
             try:
                 action = origin.player_actions[i]
             except IndexError:
                 action = model.NoAction()
             self._buttons_images.append(PhotoImage(
-                width=75, height=75,
+                width=img_size, height=img_size,
                 file=action.image
             ))
             btn = Button(
@@ -167,11 +178,9 @@ class Game(Frame):
 
     def change_image(self, img_path: str | Path) -> None:
         self._image = PhotoImage(
-            width=560, height=560,
+            width=self._screen_size, height=self._screen_size,
             file=img_path,
         )
-        # self.screen['image'] = self._image
-        # self.screen.config(image=self._image)
         self.screen.configure(image=self._image)
         self.update_idletasks()
 
@@ -182,6 +191,7 @@ class Game(Frame):
         self.update()
 
 
+
 if __name__ == '__main__':
     root = RootWidget()
 
@@ -190,7 +200,7 @@ if __name__ == '__main__':
     yara = model.Creature(model.cat_kind, 'Яра')
     root.mainframe = Game(root, yara)
     root.mainframe.change_message('\n'.join(str(act) for act in yara.player_actions))
-    root.mainframe.change_image(r'd:\G-Doc\TOP Academy\Python web\321\projects\2\_ref\doc\.img_refs\dogs2.png')
+    root.mainframe.change_image(r'd:\G-Doc\TOP Academy\Python web\321\projects\2\_ref\doc\.img_refs\dog_1.png')
     root.mainframe.update_creature(yara)
 
     root.mainloop()
